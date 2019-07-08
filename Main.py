58a96ca828223c567454cbecd2d24a8914b19e4a
Singizin <48468145+Singizin@users.noreply.github.com>
2019-07-09 01:53:22 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,13 +8,18 @@ from check import now
 fma1 = 'https://www.nstu.ru/enrollee/entrance/entrance_list?competition=4397'  # Энергетика
 fen1 = 'https://www.nstu.ru/enrollee/entrance/entrance_list?competition=4414'  # Энергетика
 fma3 = 'https://www.nstu.ru/enrollee/entrance/entrance_list?competition=4399'  # Автоматизация
-
+proxy = '78.186.140.120:58177'
+apihelper.proxy = {'https': 'https://{}'.format(proxy)}
+# TOKEN = '747611758:AAEpFP3iLMCbtmrLF0omSyTnjP7d7CCIaPY'
 TOKEN = '854025714:AAH9Wi3_rWfVJvjnbDgNWkL8hYCbH2Fr-wY'
 bot = telebot.TeleBot(TOKEN)
-print('lololo')
+i = 0
 bot.send_message(260119686, "Go")
+
 global user
 user = {}
+global what
+what = {}
 
 @bot.message_handler(commands=['fen'])
 def command_handler(message: Message):
@@ -25,9 +30,10 @@ def command_handler(message: Message):
 
 @bot.message_handler(commands=['check'])
 def start(m):
+    user.update({m.chat.id : '0'})
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add('Направления', 'Прайс-лист')
-    keyboard.add('Акции', 'Контакты')
+    keyboard.add('Направления', 'Сравнить с')
+    keyboard.add('/fen', '/check')
     msg = bot.send_message(m.chat.id, 'Выберите функцию',
                            reply_markup=keyboard)
     bot.register_next_step_handler(msg, name)
@@ -35,9 +41,18 @@ def start(m):
 
 def name(m):
     if m.text == 'Направления':
+        what.update({m.chat.id:1})
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add('13.03.02')
-        keyboard.add('15.03.04')
+        keyboard.add('13.03.02','15.03.04')
+        keyboard.add('/check')
+        msg = bot.send_message(m.chat.id, 'Выберите направление',
+                               reply_markup=keyboard)
+        bot.register_next_step_handler(msg, spisok)
+    if m.text == 'Сравнить':
+        what.update({m.chat.id: 1})
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add('13.03.02','15.03.04')
+        keyboard.add('/check')
         msg = bot.send_message(m.chat.id, 'Выберите направление',
                                reply_markup=keyboard)
         bot.register_next_step_handler(msg, spisok)
@@ -51,46 +66,40 @@ def spisok(m):
         keyboard.add('контракт', 'ориг+согсасие')
         msg = bot.send_message(m.chat.id, 'какой список?', reply_markup=keyboard)
         bot.register_next_step_handler(msg, zapros)
-    elif m.text == '15.03.04':
-        user.update({m.chat.id : '2'})
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add('копии', 'оригиналы')
-        keyboard.add('контракт', 'ориг+согсасие')
-        msg = bot.send_message(m.chat.id, 'какой список?', reply_markup=keyboard)
-        bot.register_next_step_handler(msg, zapros)
+
 
 def zapros(m):
+    msg = bot.send_message('Выберите список')
     if user.get(m.chat.id) == '1':
         url = fma1
-        napr ='#энергетика'
     elif user.get(m.chat.id) == '2':
         url = fma3
-        napr ='#автоматизация'
     if m.text == 'копии':
-        bot.send_message(m.chat.id, "{} копии:".format(napr))
-        bot.send_message(m.chat.id, now(url))
+        if what.get(m.chat.id) == 1:
+            bot.send_message(m.chat.id, now(url))
     elif m.text == 'оригиналы':
-        bot.send_message(m.chat.id, "{} оригиналы:".format(napr))
-        bot.send_message(m.chat.id, now(url + '&o_only=1'))
+        if what.get(m.chat.id) == 1:
+            bot.send_message(m.chat.id, now(url + '&o_only=1'))
     elif m.text == 'ориг+согласие':
-        bot.send_message(m.chat.id, "{} огириналы+согласие".format(napr))
-        bot.send_message(m.chat.id, now(url + '&o_only=2'))
+        if what.get(m.chat.id) == 1:
+            bot.send_message(m.chat.id, now(url + '&o_only=2'))
     elif m.text == 'контракт':
-        bot.send_message(m.chat.id, "{} контракт".format(napr))
-        bot.send_message(m.chat.id, now(url + '&o_only=3'))
-    user.update({m.chat.id:'0'})
-    bot.send_message(m.chat.id, "/fen \n /check")
-
-@bot.message_handler(content_types=['text'])
-@bot.edited_message_handler(content_types=['text'])
-def echo_digits(message: Message):
-    old = list(message.text.split(', '))
-    otvet = a(1, fma1, old)
-    print(otvet)
-    bot.send_message(260119686, "{}".format(otvet))
+        if what.get(m.chat.id) == 1:
+            bot.send_message(m.chat.id, now(url + '&o_only=3'))
+    if what.get(m.chat.id) == 2:
+        bot.register_next_step_handler(msg, compare)
 
 
-# bot.enable_save_reply_handlers(delay=1)
+def compare(m):
+    if user.get(m.chat.id) == '1':
+        url = fma1
+    elif user.get(m.chat.id) == '2':
+        url = fma3
+    old = list(m.text.split(', '))
+    otvet = a(1, url, old)
+    bot.send_message(m.chat.id, "{}".format(otvet))
 
+
+bot.enable_save_reply_handlers(delay=1)
 
 bot.polling(timeout=60)
